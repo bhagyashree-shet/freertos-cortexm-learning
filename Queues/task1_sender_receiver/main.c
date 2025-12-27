51b1f1ac3f48b3defa84eeee5d4975dbb74c83c1
bhagyashree-shet <bhagyashreeshet9927@gmail.com>
2025-12-27 18:35:21 +0530

    main.c

---
+++ Queues/task1_sender_receiver/main.c
@@ -0,0 +1,48 @@
+#include "FreeRTOS.h"
+#include "task.h"
+#include "queue.h"
+
+void SendQueue(void *params);
+void ReceiveQueue(void *params);
+
+static QueueHandle_t xQueue; /*Initialise  Queuehandle*/
+
+int main(void){
+
+xQueue = xQueueCreate( 5 , sizeof(int)); /*Create queue*/
+
+if (xQueue == NULL){
+        for(;;);  /*Check Queue Empty*/
+}
+
+
+xTaskCreate(SendQueue , "SendQueue" , 128 , NULL ,1 ,NULL);
+xTaskCreate(ReceiveQueue , "ReceiveQueue" , 128 , NULL ,1 ,NULL);
+
+vTaskStartScheduler();
+
+}
+
+void SendQueue(void *params){
+int value = 0;
+while(1){
+    value++;
+    xQueueSend(xQueue , &value , portMAX_DELAY);
+    vTaskDelay(pdMS_TO_TICKS(1000));
+}
+
+}
+
+void ReceiveQueue(void *params){
+int received;
+while(1){
+    xQueueReceive(xQueue , &received , portMAX_DELAY); /* BLOCK here until data is available */
+    __asm volatile("nop");
+
+}
+
+}
+/* Error Hooks */
+void vApplicationStackOverflowHook(TaskHandle_t xTask, char *pcTaskName) { while(1); }
+void vApplicationMallocFailedHook(void) { while(1); }
+void vApplicationTickHook(void) {}
