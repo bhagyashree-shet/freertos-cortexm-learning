019a580ca156ef083f57b0d7449b5678cbcd72fc
bhagyashree-shet <bhagyashreeshet9927@gmail.com>
2025-12-30 16:01:30 +0530

    Update main.c

--- Mutex/task1_basic/main.c
+++ Mutex/task1_basic/main.c
@@ -2,49 +2,35 @@
 #include "task.h"
 #include "semphr.h"
 
-static SemaphoreHandle_t xMutex;
-void TaskLow(void *params);
-void TaskHigh(void *params);
+SemaphoreHandle_t xMutex;
 
-int main(void){
-
-xMutex = xSemaphoreCreateMutex();
-
-
-xTaskCreate(TaskLow , "TaskLow" , 128 , NULL , 1 , NULL);
-xTaskCreate(TaskHigh , "TaskHigh" , 128 , NULL , 3 , NULL);
-vTaskStartScheduler();
-
-for(;;);
-
-}
-
-
-void TaskLow(void *params)
-{
-    while (1)
-    {   xSemaphoreTake(xMutex, portMAX_DELAY);
-
-        /* Hold mutex long enough */
-        vTaskDelay(pdMS_TO_TICKS(1000));
-
-        xSemaphoreGive(xMutex);
-        vTaskDelay(pdMS_TO_TICKS(5000));
-    }
-}
-
-void TaskHigh(void *params)
-{
-    /* Let TaskLow run first */
-    vTaskDelay(pdMS_TO_TICKS(10));
-
-    while (1)
-     {   // Let TaskLow take mutex first
-    //vTaskDelay(pdMS_TO_TICKS(50));
+void TaskLow(void *params) {
+    for(;;) {
         xSemaphoreTake(xMutex, portMAX_DELAY);
+        // This loop must be long enough for TaskHigh to wake up while we are here
+        for(volatile uint32_t i = 0; i < 2000000000; i++);
+        xSemaphoreGive(xMutex);
+        vTaskDelay(pdMS_TO_TICKS(100));
+    }
+}
+
+void TaskHigh(void *params) {
+    vTaskDelay(pdMS_TO_TICKS(50)); // Wake up while TaskLow is in the middle of its loop
+    for(;;) {
+        xSemaphoreTake(xMutex, portMAX_DELAY); // This will trigger Inheritance
         xSemaphoreGive(xMutex);
         vTaskDelay(pdMS_TO_TICKS(1000));
     }
 }
 
+int main(void) {
+    xMutex = xSemaphoreCreateMutex();
+
+    // Low priority = 1, High priority = 3
+    xTaskCreate(TaskLow, "TaskLow", 128, NULL, 1, NULL);
+    xTaskCreate(TaskHigh, "TaskHigh", 128, NULL, 3, NULL);
+
+    vTaskStartScheduler();
+    return 0;
+}
 
